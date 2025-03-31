3d7bc6d96031bb51090f6c8d422953a699765c49
andromeda <jen@andromeda.dev>
2025-03-30 20:43:31 -0400

    fix: mia's party to include castform

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -6270,7 +6270,7 @@ static const struct TrainerMonNoItemDefaultMoves sParty_GinaAndMia1[] = {
     {
     .iv = 0,
     .lvl = 6,
-    .species = SPECIES_SEEDOT,
+    .species = SPECIES_CASTFORM,
     },
     {
     .iv = 0,
